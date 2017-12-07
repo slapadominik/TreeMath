0f08e381c9093b809681362dcd5cd45feb335452
Dominik Slapa <dominik.slapa@gmail.com>
2017-12-07 15:15:06 +0100

    SmiesznaKlasa made

---
+++ SmiesznaKlasa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Domicik on 07.12.2017.
+//
+
+#include "SmiesznaKlasa.h"
---
+++ SmiesznaKlasa.h
@@ -0,0 +1,14 @@
+//
+// Created by Domicik on 07.12.2017.
+//
+
+#ifndef TREEMATH_SMIESZNAKLASA_H
+#define TREEMATH_SMIESZNAKLASA_H
+
+
+class SmiesznaKlasa {
+
+};
+
+
+#endif //TREEMATH_SMIESZNAKLASA_H
